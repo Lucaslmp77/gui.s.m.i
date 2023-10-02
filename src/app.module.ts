6cd4f0e38a260124bed65f41bf8e0a12f806e6fd
Lucas Mendes <lucas.lmp77@gmail.com>
2023-10-02 15:41:43 -0300

    Adicionado pacote de autenticação auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { PrismaService } from './prisma/prisma.service'
 import { ConfigModule } from '@nestjs/config'
 import { CreateAccountsController } from './controllers/create-account.controller'
 import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [ConfigModule.forRoot({
     validate: env => envSchema.parse(env),
     isGlobal: true,
-  })],
+  }),
+  AuthModule,
+],
   controllers: [CreateAccountsController],
   providers: [PrismaService],
 })
