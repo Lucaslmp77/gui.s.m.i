5d84a7fad10009cceb82318f02e99a562d163b94
Lucas Mendes <lucas.lmp77@gmail.com>
2023-10-10 19:53:24 -0300

    Adicionado current-user, model character, controller

---
+++ src/controllers/character.controller.ts
@@ -0,0 +1,47 @@
+import { Body, Controller, HttpCode, Post, UseGuards, UsePipes } from "@nestjs/common"
+import { PrismaService } from "src/prisma/prisma.service"
+import { z } from 'zod'
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe"
+import { AuthGuard } from "@nestjs/passport"
+import { CurrentUser } from "src/auth/current-user-decorator"
+import { UserPayload } from "src/auth/jwt.strategy"
+
+const characterBodySchema = z.object({
+    name: z.string(),
+    race: z.string(),
+    group: z.string(),
+    level: z.number(),
+    attributes: z.string(),
+    abilities: z.string(),
+})
+
+type CharacterBodySchema = z.infer<typeof characterBodySchema>
+
+@Controller('api/character')
+@UseGuards(AuthGuard('jwt'))
+export class CharacterController {
+    constructor(private prisma: PrismaService) {}
+
+    @Post()
+    @HttpCode(201)
+    @UsePipes(new ZodValidationPipe(characterBodySchema))
+    async handle(
+        @Body() body: CharacterBodySchema,
+        @CurrentUser() user: UserPayload,
+        ) {
+        const {name, race, group, level, attributes, abilities } = body
+        const authorId = user.sub
+
+        await this.prisma.character.create({
+            data: {
+                userId: authorId,
+                name: 'Lucas',
+                race: 'Lucas',
+                group: 'Lucas',
+                level: 7,
+                attributes: 'Lucas',
+                abilities: 'Lucas',
+            },
+        })
+    }
+}
