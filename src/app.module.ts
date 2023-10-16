22bc816f1484b21bc76aac7ea06966da245f7b7f
Lucas Mendes <lucas.lmp77@gmail.com>
2023-10-16 00:51:40 -0300

    Adicionado crud a controller de RpgGame

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CharacterController } from './controllers/character.controller'
+import { GameRuleController } from './controllers/game-rule.controller'
+import { PermissionController } from './controllers/permission.controller'
+import { RpgGameController } from './controllers/rpg-game.controller'
 
 @Module({
   imports: [ConfigModule.forRoot({
@@ -14,7 +17,16 @@ import { CharacterController } from './controllers/character.controller'
   }),
   AuthModule,
 ],
-  controllers: [CreateAccountsController, AuthenticateController, CharacterController],
-  providers: [PrismaService],
+  controllers: [
+    CreateAccountsController,
+    AuthenticateController, 
+    CharacterController, 
+    GameRuleController, 
+    PermissionController,
+    RpgGameController
+  ],
+  providers: [
+    PrismaService
+  ],
 })
 export class AppModule {}
